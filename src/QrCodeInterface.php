a075b6efebfaa3d5ada71a91974d07f3e91ca25e
GWenXin <35371618+GWenXin@users.noreply.github.com>
2019-06-07 12:11:09 +0800

    Update QrCodeInterface.php

--- src/QrCodeInterface.php
+++ src/QrCodeInterface.php
@@ -90,4 +90,51 @@ interface QrCodeInterface
      * @return $this
      */
     public function merge($image, $percentage = .2);
+    /**
+     * Sets the curve width and height of the qrcode.
+     *
+     * @param int $curve_width
+     * @param int $curve_height
+     * 
+     * @return $this
+     */
+    public function curve($curve_width, $curve_height);
+
+    /**
+     * Merges an icon with the center of the QrCode.
+     *
+     * @param $icon string The filepath to an icon
+     *
+     * @return $this
+     */
+    public function merge_icon($merge_icon);
+
+    /**
+     * Upload frame.
+     *
+     * @param $frame string The filepath to the frame
+     * 
+     */
+    public function frame($frame);
+
+    /**
+     * Sets the size of the frame width and height.
+     *
+     * @param int $frame_width
+     * @param int $frame_height
+     * 
+     * @return $this
+     */
+    public function frame_size($frame_width, $frame_height);
+
+    /**
+     * Sets the position x and position y of merge qrcode in the frame.
+     *
+     * @param int $position_x
+     * @param int $ position_y
+     * 
+     * @return $this
+     */
+    public function position($position_x, $position_y);
+
 }
